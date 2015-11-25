d4da1cb16264dba9770615792f55e5f08f09e91b
Alex Booker <alexbooker@fastmail.im>
2015-11-25 18:43:28 +0000

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
+
 set -e
 
-# General-purpose Yes/No prompt function. 
 # Source: https://gist.github.com/davejamesmiller/1965569
 ask() {
   while true; do
@@ -28,20 +28,71 @@ ask() {
 
 dir=`pwd`
 
-ask "Install symlink for .zshrc?" Y && ln -sf ${dir}/.zshrc ${HOME}/.zshrc
-ask "Install symlink for .zprofile?" Y && ln -sf ${dir}/.zprofile ${HOME}/.zprofile
-ask "Install symlink for .yaourtrc?" Y && ln -sf ${dir}/.yaourtrc ${HOME}/.yaourtrc
-ask "Install symlink for .Xresources?" Y && ln -sf ${dir}/.Xresources ${HOME}/.Xresources
-ask "Install symlink for .xinitrc?" Y && ln -sf ${dir}/.xinitrc ${HOME}/.xinitrc
-ask "Install symlink for .gtkrc-2.0?" Y && ln -sf ${dir}/.gtkrc-2.0 ${HOME}/.gtkrc-2.0
-ask "Install symlink for .i3blocks.conf?" Y && ln -sf ${dir}/.i3blocks.conf ${HOME}/.i3blocks.conf
-ask "Install symlink for .scripts?" Y && ln -sfn ${dir}/.scripts ${HOME}/.scripts
-ask "Install symlink for .i3?" Y && ln -sfn ${dir}/.i3 ${HOME}/.i3
-ask "Install symlink for .config/nvim?" Y && ln -sfn ${dir}/.config/nvim ${HOME}/.config/nvim
-ask "Install symlink for .config/base16-shell?" Y && ln -sfn ${dir}/.config/base16-shell ${HOME}/.config/base16-shell
-ask "Install symlink for .config/dunst?" Y && ln -sfn ${dir}/.config/dunst ${HOME}/.config/dunst
-ask "Install symlink for .config/gtk-2.0?" Y && ln -sfn ${dir}/.config/gtk-2.0 ${HOME}/.config/gtk-2.0
-ask "Install symlink for .config/gtk-3.0?" Y && ln -sfn ${dir}/.config/gtk-3.0 ${HOME}/.config/gtk-3.0
-ask "Install symlink for .config/pulse?" Y && ln -sfn ${dir}/.config/pulse ${HOME}/.config/pulse
-ask "Install symlink for .config/imgur-screenshot?" Y && ln -sfn ${dir}/.config/imgur-screenshot ${HOME}/.config/imgur-screenshot
-ask "Install symlink for .config/pulseaudio-ctl?" Y && ln -sfn ${dir}/.config/pulseaudio-ctl ${HOME}/.config/pulseaudio-ctl
+
+if ask "Install symlink for .zshrc?" Y; then
+  ln -sf ${dir}/.zshrc ${HOME}/.zshrc
+fi
+
+if ask "Install symlink for .zprofile?" Y; then
+  ln -sf ${dir}/.zprofile ${HOME}/.zprofile
+fi
+
+if ask "Install symlink for .yaourtrc?" Y; then
+  ln -sf ${dir}/.yaourtrc ${HOME}/.yaourtrc
+fi
+
+if ask "Install symlink for .Xresources?" Y; then
+  ln -sf ${dir}/.Xresources ${HOME}/.Xresources
+fi
+
+if ask "Install symlink for .xinitrc?" Y; then
+  ln -sf ${dir}/.xinitrc ${HOME}/.xinitrc
+fi
+
+if ask "Install symlink for .gtkrc-2.0?" Y; then
+  ln -sf ${dir}/.gtkrc-2.0 ${HOME}/.gtkrc-2.0
+fi
+
+if ask "Install symlink for .i3blocks.conf?" Y; then
+  ln -sf ${dir}/.i3blocks.conf ${HOME}/.i3blocks.conf
+fi
+
+if ask "Install symlink for .scripts?" Y; then
+  ln -sfn ${dir}/.scripts ${HOME}/.scripts
+fi
+
+if ask "Install symlink for .i3?" Y; then
+  ln -sfn ${dir}/.i3 ${HOME}/.i3
+fi
+
+if ask "Install symlink for .config/nvim?" Y; then
+  ln -sfn ${dir}/.config/nvim ${HOME}/.config/nvim
+fi
+
+if ask "Install symlink for .config/base16-shell?" Y; then
+  ln -sfn ${dir}/.config/base16-shell ${HOME}/.config/base16-shell
+fi
+
+if ask "Install symlink for .config/dunst?" Y; then
+  ln -sfn ${dir}/.config/dunst ${HOME}/.config/dunst
+fi
+
+if ask "Install symlink for .config/gtk-2.0?" Y; then
+  ln -sfn ${dir}/.config/gtk-2.0 ${HOME}/.config/gtk-2.0
+fi
+
+if ask "Install symlink for .config/gtk-3.0?" Y; then
+  ln -sfn ${dir}/.config/gtk-3.0 ${HOME}/.config/gtk-3.0
+fi
+
+if ask "Install symlink for .config/pulse?" Y; then
+  ln -sfn ${dir}/.config/pulse ${HOME}/.config/pulse
+fi
+
+if ask "Install symlink for .config/imgur-screenshot?" Y; then
+  ln -sfn ${dir}/.config/imgur-screenshot ${HOME}/.config/imgur-screenshot
+fi
+
+if ask "Install symlink for .config/pulseaudio-ctl?" Y; then
+  ln -sfn ${dir}/.config/pulseaudio-ctl ${HOME}/.config/pulseaudio-ctl
+fi
