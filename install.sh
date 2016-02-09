e60346de10cb71a586d4535fd14d4d86fdf07a4d
Alex Booker <alexbooker@fastmail.im>
2016-02-09 17:13:58 +0000

    Update dotfiles

--- install.sh
+++ install.sh
@@ -28,12 +28,20 @@ ask() {
 
 dir=`pwd`
 
-if ask "Install symlink for .zshrc?" Y; then
-  ln -sf ${dir}/.zshrc ${HOME}/.zshrc
+if ask "Install symlink for .config/nvim?" Y; then
+  ln -sfn ${dir}/.config/nvim ${HOME}/.config/nvim
+fi
+
+if ask "Install symlink for .config/base16-shell?" Y; then
+  ln -sfn ${dir}/.config/base16-shell ${HOME}/.config/base16-shell
 fi
 
 if ask "Install symlink for .tmux.conf?" Y; then
   ln -sf ${dir}/.tmux.conf ${HOME}/.tmux.conf
+fi
+
+if ask "Install symlink for .zshrc?" Y; then
+  ln -sf ${dir}/.zshrc ${HOME}/.zshrc
 fi
 
 if ask "Install symlink for .zprofile?" Y; then
@@ -52,10 +60,6 @@ if ask "Install symlink for .xinitrc?" Y; then
   ln -sf ${dir}/.xinitrc ${HOME}/.xinitrc
 fi
 
-if ask "Install symlink for .gtkrc-2.0?" Y; then
-  ln -sf ${dir}/.gtkrc-2.0 ${HOME}/.gtkrc-2.0
-fi
-
 if ask "Install symlink for .i3blocks.conf?" Y; then
   ln -sf ${dir}/.i3blocks.conf ${HOME}/.i3blocks.conf
 fi
@@ -66,36 +70,4 @@ fi
 
 if ask "Install symlink for .i3?" Y; then
   ln -sfn ${dir}/.i3 ${HOME}/.i3
-fi
-
-if ask "Install symlink for .config/nvim?" Y; then
-  ln -sfn ${dir}/.config/nvim ${HOME}/.config/nvim
-fi
-
-if ask "Install symlink for .config/base16-shell?" Y; then
-  ln -sfn ${dir}/.config/base16-shell ${HOME}/.config/base16-shell
-fi
-
-if ask "Install symlink for .config/dunst?" Y; then
-  ln -sfn ${dir}/.config/dunst ${HOME}/.config/dunst
-fi
-
-if ask "Install symlink for .config/gtk-2.0?" Y; then
-  ln -sfn ${dir}/.config/gtk-2.0 ${HOME}/.config/gtk-2.0
-fi
-
-if ask "Install symlink for .config/gtk-3.0?" Y; then
-  ln -sfn ${dir}/.config/gtk-3.0 ${HOME}/.config/gtk-3.0
-fi
-
-if ask "Install symlink for .config/pulse?" Y; then
-  ln -sfn ${dir}/.config/pulse ${HOME}/.config/pulse
-fi
-
-if ask "Install symlink for .config/imgur-screenshot?" Y; then
-  ln -sfn ${dir}/.config/imgur-screenshot ${HOME}/.config/imgur-screenshot
-fi
-
-if ask "Install symlink for .config/pulseaudio-ctl?" Y; then
-  ln -sfn ${dir}/.config/pulseaudio-ctl ${HOME}/.config/pulseaudio-ctl
 fi
